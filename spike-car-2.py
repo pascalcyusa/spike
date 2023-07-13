c8c01938e2105f905439b60a2dda9dc35417bce5
Jean Pascal Cyusa Shyaka <shyakaadolf@gmail.com>
2023-07-13 16:16:40 -0400

    updated the way motors move

--- spike-car-2.py
+++ spike-car-2.py
@@ -6,7 +6,7 @@ import runloop
 
 
 async def main():
-    # Move straight at default velocity
+    # Move forward at default velocity
     motor.run(port.A, -500)
     motor.run(port.B, 500)
     motor.run(port.C, 500)
@@ -15,24 +15,21 @@ async def main():
     while color_sensor.color(port.E) != color.GREEN or color_sensor.color(port.E) != color.BLACK:
         # When front sensor detects green or black, stop and move backward
         if color_sensor.color(port.E) == color.GREEN or color_sensor.color(port.E) == color.BLACK:
-            motor.stop(port.A)
             motor.stop(port.B)
             motor.stop(port.C)
             motor.stop(port.D)
-            # Wait for 1 second
-            await runloop.sleep_ms(1000)
+            # Move forward at backward velocity
             motor.run(port.A, 500)
             motor.run(port.B, -500)
             motor.run(port.C, -500)
             motor.run(port.D, 500)
-        # Whwn back sensor detects green or black, stop and move forward
+            motor.stop(port.A)
+        # When back sensor detects green or black, stop and move forward
         if color_sensor.color(port.F) == color.GREEN or color_sensor.color(port.F) == color.BLACK:
             motor.stop(port.A)
             motor.stop(port.B)
             motor.stop(port.C)
             motor.stop(port.D)
-            # Wait for 1 second
-            await runloop.sleep_ms(1000)
             # Move forward at default velocity
             motor.run(port.A, -500)
             motor.run(port.B, 500)
