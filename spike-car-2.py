c3f94cd8fbe3c02d021bd0b539d6bc6757791065
Jean Pascal Cyusa Shyaka <shyakaadolf@gmail.com>
2023-07-14 12:38:26 -0400

    removed the stops functions

--- spike-car-2.py
+++ spike-car-2.py
@@ -13,24 +13,15 @@ async def main():
     motor.run(port.D, -500)
 
     while color_sensor.color(port.E) != color.GREEN or color_sensor.color(port.E) != color.BLACK:
-        # When front sensor detects green or black, stop and move backward
+        # When front sensor detects a color, stop and reverse direction
         if color_sensor.color(port.E) == color.GREEN or color_sensor.color(port.E) == color.BLACK:
-            motor.stop(port.B)
-            motor.stop(port.C)
-            motor.stop(port.D)
-            # Move forward at backward velocity
             motor.run(port.A, 500)
             motor.run(port.B, -500)
             motor.run(port.C, -500)
             motor.run(port.D, 500)
-            motor.stop(port.A)
-        # When back sensor detects green or black, stop and move forward
+
+        # When back sensor detects a color, stop and reverse direction
         if color_sensor.color(port.F) == color.GREEN or color_sensor.color(port.F) == color.BLACK:
-            motor.stop(port.A)
-            motor.stop(port.B)
-            motor.stop(port.C)
-            motor.stop(port.D)
-            # Move forward at default velocity
             motor.run(port.A, -500)
             motor.run(port.B, 500)
             motor.run(port.C, 500)
